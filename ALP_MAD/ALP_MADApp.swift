2f5fd918b62eee83752a6eac158a9307a0c739ff
rafi <rafinaya117@gmail.com>
2024-05-23 22:55:57 +0700

    Initial Commit

---
+++ ALP_MAD/ALP_MADApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_MADApp.swift
+//  ALP_MAD
+//
+//  Created by zenzen on 23/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ALP_MADApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
