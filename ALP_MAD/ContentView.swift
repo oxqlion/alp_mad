77cac2950e97d827e848b95d1f96c6dad4f2bad5
rafi <rafinaya117@gmail.com>
2024-05-23 23:00:16 +0700

    Hello Joren and Friends 2.0 :D

--- ALP_MAD/ContentView.swift
+++ ALP_MAD/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Joren and Friends 2.0 :D!")
         }
         .padding()
     }
